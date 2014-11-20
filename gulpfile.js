f472be700d6a953eb198df98473e214e6a3e0e93
Liz Starin <liz.starin@gmail.com>
2014-11-20 16:52:12 -0500

    adds gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var concatFilenames = require('gulp-concat-filenames');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['file-manifest']);
+
+var concatFilenamesOptions = {
+    root: './assets',
+};
+
+gulp.task('file-manifest', function() {
+  gulp
+  .src('./assets/*')
+  .pipe(concatFilenames('manifest.txt', concatFilenamesOptions))
+  .pipe(gulp.dest('./js/'));
 });
